7180bf06f1d0e19a247d53f85a042921fb596e3c
Radif Sharafullin <radif.sharafullin@gmail.com>
2013-09-11 20:12:30 -0700

    remonter about canonical path

--- mcbPlatformSupport/mcbPlatformSupportFunctions.h
+++ mcbPlatformSupport/mcbPlatformSupportFunctions.h
@@ -50,6 +50,7 @@ namespace mcb{namespace PlatformSupport{namespace Functions{
     std::string stringByDeletingLastPathComponent(const std::string & path);
     std::string deletePathExtension(const std::string & path);
     std::string stringByAppendingPathComponent(std::string path, std::string component);
+    //TODO: Canonical path
     void _removeLastSlashInPath(std::string & path);
     //parser
     
