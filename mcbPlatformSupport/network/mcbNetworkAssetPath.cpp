cc89aca53354459c6a0bee9d827391425ec80451
Radif Sharafullin <radif.sharafullin@gmail.com>
2014-01-07 01:29:03 -0800

    network asset path experimentation

--- mcbPlatformSupport/network/mcbNetworkAssetPath.cpp
+++ mcbPlatformSupport/network/mcbNetworkAssetPath.cpp
@@ -8,5 +8,23 @@
 
 #include "mcbNetworkAssetPath.h"
 namespace mcb{namespace PlatformSupport{namespace network{
+    void NetworkAssetManager::init(){
     
+    }
+    std::string NetworkAssetManager::resolveNetworkAssetPath(const std::string & path){
+        std::string localPath;
+        std::string networkURL;
+        
+        //check the data, see if can find it, if not, return local path, otherwise, return path from data, if exists
+        
+        //enqeue:
+        //compare versions, start download, unzip
+        
+        //remove previous version, update data
+        
+    }
+    void NetworkAssetManager::loadVersionsWithPath(const std::string & path){
+        std::string dataPath(resolveNetworkAssetPath(path));
+        
+    }
 }}}
--- mcbPlatformSupport/network/mcbNetworkAssetPath.h
+++ mcbPlatformSupport/network/mcbNetworkAssetPath.h
@@ -9,8 +9,35 @@
 #ifndef __SoundSurfer__mcbNetworkAssetPath__
 #define __SoundSurfer__mcbNetworkAssetPath__
 
-#include <iostream>
+#include "mcbFactory.h"
+#include "mcbPath.h"
+
 namespace mcb{namespace PlatformSupport{namespace network{
+    class NetworkAssetManager : public SingletonFactory<NetworkAssetManager>{
+    public:
+        virtual void init() override;
+        
+        //track file versions
+        //local versions, compare and resolve path
+        
+        //initWithVersions
+        
+        void loadVersionsWithPath(const std::string & path);
+        
+        //path format:
+        //remote | local
+        //$(HTTP)http://path.com?file.zip|$(SHARED)/store
+        std::string resolveNetworkAssetPath(const std::string & path);
+        
+    };
     
+    class NetworkAssetPath : public virtual Path{
+    protected:
+        std::string mcbNetworkAssetPath(const std::string & inPath){return mcbPath(NetworkAssetManager::sharedInstance()->resolveNetworkAssetPath(inPath));}
+        cocos2d::CCString * mcbNetworkAssetPath(const cocos2d::CCString * inPath){return cocos2d::CCString::create(mcbNetworkAssetPath(inPath->m_sString));}
+    };
+    
+   
+   
 }}}
 #endif /* defined(__SoundSurfer__mcbNetworkAssetPath__) */
