80b47f419c1f7aeca61cf8ab10f949cd41f1d30a
Radif Sharafullin <radif@Radifs-MacBook-Pro.local>
2016-01-19 18:50:59 -0800

    focus engine

--- mcbPlatformSupport/mcbFocus/mcbFocusEngine.cpp
+++ mcbPlatformSupport/mcbFocus/mcbFocusEngine.cpp
@@ -8,19 +8,165 @@
 
 #include "mcbFocusEngine.hpp"
 namespace mcb{namespace PlatformSupport{
+    using namespace cocos2d;
     void FocusEngine::init(){
         
     }
     
-    void FocusEngine::addFocusableNode(cocos2d::CCNode * node, const std::function<void(cocos2d::CCNode * node)> action){
-        
+    void FocusEngine::addFocusableNode(cocos2d::CCNode * node, const std::function<void(cocos2d::CCNode * node, const bool isFocused, bool animated)> & focusAction, const std::function<void(cocos2d::CCNode * node)> & triggerAction){
+        removeFocusableNode(node);
+        _focusables.insert({node, Focusable{node, focusAction, triggerAction}});
     }
     
     void FocusEngine::removeFocusableNode(cocos2d::CCNode * node){
-        
+        auto it(_focusables.find(node));
+        if (it!=_focusables.end())
+            _focusables.erase(it);
     }
     
     void FocusEngine::removeAllFocusableNodes(){
+        _focusables.erase(_focusables.begin(), _focusables.end());
+    }
+    
+    //sorting the matrix
+    void FocusEngine::sortByCurrentPositions(){
+        struct WorldPositionNode{CCNode *node;CCPoint worldPos;};
+        std::vector<WorldPositionNode> nodes;
+        nodes.reserve(_focusables.size());
+        for (const auto & p : _focusables)
+            nodes.emplace_back(WorldPositionNode{p.first, p.first->getParent()->convertToWorldSpace(p.first->getPosition())});
         
+        //nullable accessor if exceeds index, !!!does not check if less than 0!
+        const auto nodeAtIndexL([&](const int index)->CCNode *{
+            if (index<nodes.size())
+                return nodes[index].node;
+            return nullptr;
+        });
+        
+        //SORTING HORIZONTALLY (x)
+        std::sort(nodes.begin(), nodes.end(), [](const WorldPositionNode & node1, const WorldPositionNode & node2){return node1.worldPos.x>node2.worldPos.x;});
+        
+        
+        for (int i(0); i<nodes.size(); ++i) {
+            CCNode * currNode(nodeAtIndexL(i));
+            if (currNode) {
+                auto it(_focusables.find(currNode));
+                if (it!=_focusables.end()) {
+                    if (i==0)
+                        it->second._leftNode=nullptr;
+                    it->second._rightNode=nodeAtIndexL(i+1);
+                }
+            }
+        }
+         
+        
+        //SORTING VERTICALLY (y)
+        std::sort(nodes.begin(), nodes.end(), [](const WorldPositionNode & node1, const WorldPositionNode & node2){return node1.worldPos.y>node2.worldPos.y;});
+        
+        
+        for (int i(0); i<nodes.size(); ++i) {
+            CCNode * currNode(nodeAtIndexL(i));
+            if (currNode) {
+                auto it(_focusables.find(currNode));
+                if (it!=_focusables.end()) {
+                    if (i==0)
+                        it->second._downNode=nullptr;
+                    it->second._upNode=nodeAtIndexL(i+1);
+                }
+            }
+        }
+        
+    }
+    
+    //selection
+    
+    cocos2d::CCNode * FocusEngine::moveSelectionRight(bool animated){
+        if (_currentlySelectedNode) {
+            auto it(_focusables.find(_currentlySelectedNode));
+            if (it!=_focusables.end()) {
+                CCNode * nextNode(it->second._rightNode);
+                if (nextNode) {
+                    setCurrentlySelectedNode(nextNode, true, true);
+                    return nextNode;
+                }
+            }
+        }
+        //bail:
+        CCLog("no currently selected node!!!");
+        return nullptr;
+    }
+    cocos2d::CCNode * FocusEngine::moveSelectionLeft(bool animated){
+        if (_currentlySelectedNode) {
+            auto it(_focusables.find(_currentlySelectedNode));
+            if (it!=_focusables.end()) {
+                CCNode * nextNode(it->second._leftNode);
+                if (nextNode) {
+                    setCurrentlySelectedNode(nextNode, true, true);
+                    return nextNode;
+                }
+            }
+        }
+        //bail:
+        CCLog("no currently selected node!!!");
+        return nullptr;
+    }
+    cocos2d::CCNode * FocusEngine::moveSelectionUp(bool animated){
+        if (_currentlySelectedNode) {
+            auto it(_focusables.find(_currentlySelectedNode));
+            if (it!=_focusables.end()) {
+                CCNode * nextNode(it->second._upNode);
+                if (nextNode) {
+                    setCurrentlySelectedNode(nextNode, true, true);
+                    return nextNode;
+                }
+            }
+        }
+        //bail:
+        CCLog("no currently selected node!!!");
+        return nullptr;
+    }
+    cocos2d::CCNode * FocusEngine::moveSelectionDown(bool animated){
+        if (_currentlySelectedNode) {
+            auto it(_focusables.find(_currentlySelectedNode));
+            if (it!=_focusables.end()) {
+                CCNode * nextNode(it->second._downNode);
+                if (nextNode) {
+                    setCurrentlySelectedNode(nextNode, true, true);
+                    return nextNode;
+                }
+            }
+        }
+        //bail:
+        CCLog("no currently selected node!!!");
+        return nullptr;
+    }
+    
+    void FocusEngine::setCurrentlySelectedNode(cocos2d::CCNode *node, bool withFocusAction, bool animated){
+        auto it(_focusables.find(node));
+        if (it!=_focusables.end() && !it->second.isFocused && node!=_currentlySelectedNode && node!=nullptr) {
+            //unselect current node
+            std::for_each(_focusables.begin(), _focusables.end(), [&](const std::pair<cocos2d::CCNode *, Focusable> & p){
+                    //unselect is always with focus action
+                    p.second.focus(false, animated);
+            });
+            
+            //select current node
+            _currentlySelectedNode=it->first;
+            if (withFocusAction)
+                it->second.focus(true, animated);
+            
+        }else
+            CCLog("Error! no such node registered!");
+    }
+    cocos2d::CCNode * FocusEngine::triggerCurrentSelection(){
+        if (_currentlySelectedNode)
+            triggerSelectionForNode(_currentlySelectedNode);
+        return _currentlySelectedNode;
+    }
+    
+    void FocusEngine::triggerSelectionForNode(cocos2d::CCNode * node){
+        auto it(_focusables.find(node));
+        if (it!=_focusables.end())
+            it->second.trigger();
     }
 }}
--- mcbPlatformSupport/mcbFocus/mcbFocusEngine.hpp
+++ mcbPlatformSupport/mcbFocus/mcbFocusEngine.hpp
@@ -12,15 +12,75 @@
 #include "mcbFactory.h"
 namespace mcb{namespace PlatformSupport{
     class FocusEngine : public SingletonFactory<FocusEngine>{
+        struct Focusable{
+            const cocos2d::CCNode * node;
+            const std::function<void(cocos2d::CCNode * node, const bool isFocused, bool animated)> focusAction;
+            const std::function<void(cocos2d::CCNode * node)> triggerAction;
+            
+            //ctor
+            Focusable(cocos2d::CCNode * node, const std::function<void(cocos2d::CCNode * node, const bool isFocused, bool animated)> & focusAction, const std::function<void(cocos2d::CCNode * node)> & triggerAction)
+            : node(node)
+            , focusAction(focusAction)
+            , triggerAction(triggerAction)
+            {}
+
+            //focused property managed by the wrapper class
+            bool isFocused=false;
+            
+            void focus(bool m_isFocused, bool animated) const{
+                if(m_isFocused!=isFocused){
+                    const_cast<Focusable *>(this)->isFocused=m_isFocused;
+                    if(focusAction)
+                        focusAction(const_cast<cocos2d::CCNode *>(node), isFocused, animated);
+                }
+            }
+            void trigger() const{
+            if(triggerAction)
+                triggerAction(const_cast<cocos2d::CCNode *>(node));
+            }
+            //updateable positions
+            cocos2d::CCNode * _upNode=nullptr;
+            cocos2d::CCNode * _downNode=nullptr;
+            cocos2d::CCNode * _leftNode=nullptr;
+            cocos2d::CCNode * _rightNode=nullptr;
+        };
+        
+        std::map<cocos2d::CCNode *, Focusable> _focusables;
+        
+        cocos2d::CCNode *_currentlySelectedNode=nullptr;
+        
     public:
         virtual void init() override;
         virtual ~FocusEngine()=default;
         
-        void addFocusableNode(cocos2d::CCNode * node, const std::function<void(cocos2d::CCNode * node)> action=nullptr);
+        void addFocusableNode(cocos2d::CCNode * node, const std::function<void(cocos2d::CCNode * node, const bool isFocused, bool animated)> & focusAction=nullptr, const std::function<void(cocos2d::CCNode * node)> & triggerAction=nullptr);
         
-        void removeFocusableNode(cocos2d::CCNode * node);
+        void removeFocusableNode(cocos2d::CCNode * node);//custom manupulation
         
-        void removeAllFocusableNodes();
+        void removeAllFocusableNodes();//use this when the current scene exits
+        
+        void sortByCurrentPositions();//all nodes must have parents
+        
+        float jumpingSwipeDistance=60.0f;
+        
+        //swipe touch methods
+        
+        //....
+        //TODO:
+        
+        
+        //focus shift
+        cocos2d::CCNode * moveSelectionRight(bool animated=true);
+        cocos2d::CCNode * moveSelectionLeft(bool animated=true);
+        cocos2d::CCNode * moveSelectionUp(bool animated=true);
+        cocos2d::CCNode * moveSelectionDown(bool animated=true);
+        
+        cocos2d::CCNode * currentlySelectedNode(){return _currentlySelectedNode;}
+        void setCurrentlySelectedNode(cocos2d::CCNode * node, bool withFocusAction, bool animated);
+        
+        cocos2d::CCNode * triggerCurrentSelection();
+        
+        void triggerSelectionForNode(cocos2d::CCNode * node);
     };
 }}
 #endif /* mcbFocusEngine_hpp */
